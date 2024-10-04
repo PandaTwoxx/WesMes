4a069bb994a431c6dabad1799054bb73303e6445
Weston Gorczyca <weston.gorczyca@gmail.com>
2024-10-04 17:15:17 -0400

    added client logging

--- service/routes.py
+++ service/routes.py
@@ -7,8 +7,11 @@ from flask_socketio import send, SocketIO
 # For proxies
 from werkzeug.middleware.proxy_fix import ProxyFix
 
+
+# Global/Enivironment variables
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*")
+logger = logging.getLogger('messanger')
 
 # For proxies
 app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
@@ -31,5 +34,10 @@ def send_message(message: str) -> None:
 def index():
     """The root html response
     """
-    logging.info('Client %s',request.remote_addr)
+    logger.info(
+        'Client %s connected to %s using method %s',
+        request.remote_addr,
+        request.path,
+        request.method
+    )
     return render_template("index.html")
