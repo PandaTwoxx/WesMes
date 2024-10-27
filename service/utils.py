68ef294d0080d3f9cb0197b34df7d0f90c4aca15
Weston Gorczyca <weston.gorczyca@gmail.com>
2024-10-27 08:59:02 -0400

    Fixed login system

--- service/utils.py
+++ service/utils.py
@@ -1,7 +1,5 @@
 """Utility Functions"""
 
-from http import HTTPStatus
-from flask import abort, request, redirect, url_for
 from service.routes import login_manager, r
 
 
@@ -17,21 +15,6 @@ login_manager.needs_refresh_message = (
 login_manager.needs_refresh_message_category = "info"
 login_manager.session_protection = "strong"
 
-
-
-
-@login_manager.unauthorized_handler
-def unauthorized():
-    """Endpoint for unauthorized
-    Returns:
-        Response: redirects to login page, http code 200
-        NoReturn: aborts with unauthorized status
-    """
-    if request.blueprint == "api":
-        abort(HTTPStatus.UNAUTHORIZED)
-    return redirect(url_for("login_page")), 200
-
-
 @login_manager.user_loader
 def user_loader(user_id):
     """Returns user object
@@ -43,21 +26,3 @@ def user_loader(user_id):
         User: The loaded user object
     """
     return r.hget('users', user_id)
-
-def check_username_email(username: str, email: str) -> bool:
-    """Checks if username and email dont contain $
-
-    Args:
-        username (str): The username
-        email (str): The email
-
-    Returns:
-        bool: False if the email or username contains $
-    """
-    for i, char in enumerate(username):
-        if char == '$':
-            return i
-    for i, char in enumerate(email):
-        if char == '$':
-            return i
-    return True
