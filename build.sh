09e2a2720d695e04578b60058ceeffc80d54612b
Weston Gorczyca <weston.gorczyca@gmail.com>
2024-10-27 10:09:57 -0400

    added docker up to build.sh file

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 npx tailwindcss -i ./service/input.css -o ./service/static/output.css --watch
 poetry export -f requirements.txt --output requirements.txt --without-hashes
+docker compose up --build
