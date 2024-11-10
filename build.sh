ac1262d5d349fe07c7dd58b9db45635cf5886651
Weston Gorczyca <weston.gorczyca@gmail.com>
2024-11-10 09:08:53 -0500

    Moved input.css to static/

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ npm install @tailwindcss/forms
 npm install @tailwindcss/aspect-ratio
 npm install @tailwindcss/container-queries
 npm install flowbite
-npx tailwindcss -i ./service/input.css -o ./service/static/output.css
+npx tailwindcss -i ./service/static/input.css -o ./service/static/output.css
 python3 -m venv .venv
 source .venv/bin/activate
 pip install poetry
